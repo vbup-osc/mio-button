033be6d73f774f9258658a00d8b7501d295d7057
MioButton <samuimomijiyuki@qq.com>
2020-07-15 01:08:53 +0800

    Update home.vue

--- src/components/home.vue
+++ src/components/home.vue
@@ -287,12 +287,6 @@ class HomePage extends Vue {
             this.live_data_loading = false;
         })
     }
-    format_time(){
-        var ts = arguments[0] || 0;
-        var t,y,m,d,h,i,s;
-        t = ts ? new Date(ts*1000) : new Date();y = t.getFullYear();m = t.getMonth()+1;d = t.getDate();h = t.getHours();i = t.getMinutes();s = t.getSeconds();
-        return y+'-'+(m<10?'0'+m:m)+'-'+(d<10?'0'+d:d)+' '+(h<10?'0'+h:h)+':'+(i<10?'0'+i:i)+':'+(s<10?'0'+s:s);
-    }
     play(item){
         if (this.overlapCheck) {
             let audio = new Audio("voices/" + item.path);
