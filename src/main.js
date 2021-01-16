3d833b2277bee375982b1661e19a096a93fd6675
blacktunes <blacktunes@foxmail.com>
2021-01-16 17:10:59 +0800

    移除无用文件和无用依赖

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
-
-import $ from 'jquery'
 import i18n from './assets/script/i18n'
-
-global.jQuery = global.$ = $;
-
-import 'bootstrap'
-import GlobalConst from './globalconst'
-
-Vue.use(GlobalConst);
 
 Vue.config.productionTip = false
 
